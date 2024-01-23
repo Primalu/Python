1712e4b441451bd728161434291604dc97c69aea
Primalu <157064422+Primalu@users.noreply.github.com>
2024-01-23 11:14:12 -0600

    Add files via upload

---
+++ knockknock.py
@@ -0,0 +1,24 @@
+#Variables
+x = ("...\n")
+m = ("MOOO\n")
+#One Line Joke
+print("Knock, Knock.\nWho's there?\nInturping cow!\nInturpting cow wh-\n",m,"Inturpting co-\n",m,"Inturpt-\n",m,x,x,"Is this fun for you?\nVery moooch")
+#Seprate Line Joke
+print("Knock, Knock.")
+print("Who's there?")
+print("Inturping cow!")
+print("Inturping cow wh-")
+print(m)
+print("Inturpting co-")
+print(m)
+print("Inturpt")
+print(m)
+print(x)
+print(x)
+print("This again?")
+print("I found it in the moos-paper")
+#I find both hard to read, I think a combonation would be the best
+print("Knock, Knock.\nWho's there?\nInturping cow!")
+print("Inturpting cow wh-",m,"Inturpting co-\n",m,"Inturpt-")
+print(m,x,x,"You need to stop milking this joke\nDID YOU JUST-\nNONONO, I didn't mean it and you know that!")
+#Final Thoughts, I defintly made the Variables with the one line of code in mind witch cuased the Seprate line to look a bit odd and I had to work around it in the combo.
