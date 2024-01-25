bee0c31e17c6087f406309f68586ac81f6b4716c
Primalu <157064422+Primalu@users.noreply.github.com>
2024-01-25 10:48:41 -0600

    PE1 2.3: Mathematical Operations 01-25-2024

---
+++ 0125mathematicaloperations.py
@@ -0,0 +1,9 @@
+#Preset Variables
+length = 3
+width = 9
+
+#Oprational Variables
+area = length * width
+
+#Print Statment
+print ("The area of the rectangle with a length of",length,"and a width of",width,"is ",area,".")
