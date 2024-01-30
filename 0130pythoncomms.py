0346cc3daeb9fded5c3b3c506b86532172499325
Primalu <157064422+Primalu@users.noreply.github.com>
2024-01-30 10:21:48 -0600

    PE1 2.5: Python Comments

---
+++ 0130pythoncomms.py
@@ -0,0 +1,11 @@
+
+# Variables
+tank_size = 15 # Tank size var
+distance = 600 # Distance var
+
+# Equation
+mpg = distance / tank_size # mpg var
+
+# Print Statements
+print("Miles per gallon:", mpg)
+
