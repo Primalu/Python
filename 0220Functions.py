b67689dc298a15fb84c49ddb1c3ae49bc68aa12d
Primalu <157064422+Primalu@users.noreply.github.com>
2024-02-20 10:32:13 -0600

    Add files via upload

---
+++ 0220Functions.py
@@ -0,0 +1,12 @@
+# Create Functions: Develop and define their functions in Python.
+# Send Values to Functions: Understand and practice how to pass arguments to these functions.
+ 
+# Defining functions
+def square_area(side):
+    print("The area of the square is", side * side, "square units.")
+def circle_area(radius):
+    print("The area of the circle is", 3.14 * radius * radius,"square units.")
+
+# Calling function
+square_area(88)
+circle_area(88)
