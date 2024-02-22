32139d83131e91318bcf8d73dce8f1b4c850e73b
Primalu <157064422+Primalu@users.noreply.github.com>
2024-02-22 11:38:57 -0600

    Add files via upload

---
+++ 0222scope_BMI.py
@@ -0,0 +1,8 @@
+'''
+Create a BMI calculator that takes a user's weight and height, calculates their BMI, 
+and then categorizes their BMI into underweight, normal weight, overweight, or obese.
+
+'''
+
+weight = int(input("Enter your weight in pounds: "))
+height = int(input("Enter your height in inches: "))
