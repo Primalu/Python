8197e1d7d150bd5b60e95c87588d9e1e69af5ab6
Primalu <157064422+Primalu@users.noreply.github.com>
2024-02-26 17:56:59 -0600

    Corrected spelling

--- 0222Simple_Interest_Calculator.py
+++ 0222Simple_Interest_Calculator.py
@@ -10,13 +10,13 @@ The function should calculate the simple interest using the given formula and re
 
 
 def calculate_interest():
-    principal_amount, interest_rate, time = get_user_imputs()
+    principal_amount, interest_rate, time = get_user_inputs()
     calculated_interest = (principal_amount * interest_rate * time) / 100
     print(f"The simple interest for a principal amount of ${principal_amount:,.2f} at an \ninterest rate of {interest_rate}% over a period of {time} years is: ${calculated_interest:,.2f}")
 
 
-# Getting user imputs
-def get_user_imputs():
+# Getting user inputs
+def get_user_inputs():
     principal_amount = int(input("Enter the principal amount: "))
     interest_rate = int(input("Enter the interest rate as a whole number (5% would be 5): "))
     time = int(input("Enter the investment time in whole years: "))
