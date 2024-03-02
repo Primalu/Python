e7d64ac68838c8b8a484d70cd346305eb8544247
Primalu <157064422+Primalu@users.noreply.github.com>
2024-03-02 11:55:54 -0600

    Update 0222scope_BMI.py

--- 0222scope_BMI.py
+++ 0222scope_BMI.py
@@ -4,5 +4,20 @@ and then categorizes their BMI into underweight, normal weight, overweight, or o
 
 '''
 
-weight = int(input("Enter your weight in pounds: "))
-height = int(input("Enter your height in inches: "))
+WEIGHT = int(input("Please enter your weight in pounds (in pound): "))
+HEIGHT = int(input("Please enter your height in inches (in inches): "))
+
+WeightKG = WEIGHT * 0.453592
+HeightM = HEIGHT * 0.0254
+
+BMI = WeightKG / (HeightM * HeightM)
+print(f"Your BMI is: {BMI:.2f}")
+
+if BMI < 18.5:
+    print("You are in the underweight category.")
+elif BMI < 24.9:
+    print("You are in the normal weight category.")
+elif BMI < 29.9:
+    print("You are in the overweight category.")
+else:
+    print("You are in the obese category.")
